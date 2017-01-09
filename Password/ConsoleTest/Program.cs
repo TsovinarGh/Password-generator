cd612eaaaabd015ae47329158a8cb9d8040c1474
Tsovinar <Tsovinar.ghazaryan@yahoo.com>
2017-01-09 12:23:37 +0400

    password

---
+++ Password/ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using CryptoServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i < 10; i++)
+            {
+                var pwd = Password.Generate();
+                Console.WriteLine(pwd);
+
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Password/CryptoServices/Password.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace CryptoServices
+{
+    public class Password
+    {
+        private byte[] content = new byte[16];
+
+        private Password(byte[] content)
+        {
+            this.content = content;
+        }
+
+        public override string ToString()
+        {
+            var builder = new StringBuilder();
+
+            foreach(var member in content)
+            {
+                string pairStr = member.ToString("X");
+                builder.Append(pairStr);   
+            }
+
+            return builder.ToString();
+        }
+
+        public static Password Generate()
+        {
+            Password result;
+            using (var rng = new RNGCryptoServiceProvider())
+            {
+                byte[] tempArray = new byte[16];
+                rng.GetBytes(tempArray);
+                result = new Password(tempArray);
+            }
+            return result;
+        }
+    }
+}
